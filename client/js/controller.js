678342c30929b0d9923cbb987c438ae837d9f982
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-04-27 11:01:05 -0700

    Introduce entity and do a whole bunch of refactoring

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,38 +1,17 @@
-import { rgb } from "./util";
-import { PlayerState } from "./player-state";
-import { Player } from "./player";
+import World from "./world";
 
 export default class Controller {
 
 	constructor() {
-		this.animAmt = 0;
-		this.period = 3;
-
-		this.playerState = new PlayerState();
-
-		this.player = new Player();
-		this.cameraPos = {x: 0, y: 0};
+		this.world = new World();
 	}
 
 	update(dt) {
-		this.animAmt += dt / this.period;
-		this.animAmt %= 1;
-
-		this.playerState.update(dt);
-
-		this.player.update();
-
-		this.updateCamera();
+		this.world.update(dt);
 	}
 
 	onMouseClick(clickPos) {
-		this.player.pos.x = clickPos.x + this.cameraPos.x;
-		this.player.pos.y = clickPos.y + this.cameraPos.y;
-	}
-
-	updateCamera() {
-		this.cameraPos.x += 0.02 * (this.player.pos.x - this.cameraPos.x);
-		this.cameraPos.y += 0.02 * (this.player.pos.y - this.cameraPos.y);
+		this.world.onMouseClick(clickPos);
 	}
 
 	/**
@@ -40,13 +19,7 @@ export default class Controller {
 	 * @param {!CanvasRenderingContext2D} context 
 	 */
 	render(context) {
-		context.save();
-
-		context.translate(-this.cameraPos.x, -this.cameraPos.y);
-
-		this.player.render(context);
-
-		context.restore();
+		this.world.render(context);
 	}
 
 }
--- client/js/player.js
+++ client/js/player.js
@@ -1,20 +1,31 @@
-import { getImage, drawSprite } from "./sprites";
+import { drawSprite } from "./sprites";
+import { SCALE } from "./constants";
+import { Entity } from "./entity";
 
-export class Player {
+export class Player extends Entity {
 
     constructor() {
-        this.pos = {x: 0, y: 0};
+        super();
+
         this.state = null; // player state thing
+
+        this.width = SCALE * 8;
+        this.height = SCALE * 8;
+
+        this.animCount = 0;
+        this.animPeriod = 1;
     }
 
     update(dt) {
-        // Nothing??
+        this.animCount += dt / this.animPeriod;
+        this.animCount %= 1;
     }
 
     /**
      * @param {CanvasRenderingContext2D} context 
      */
     render(context) {
-        drawSprite(context, "char", 0, this.pos.x, this.pos.y, 8, 8);
+        const frame = this.animCount < 0.5 ? 0 : 1;
+        drawSprite(context, "char", frame, this.midX, this.maxY, SCALE * 8, SCALE * 8);
     }
 }
