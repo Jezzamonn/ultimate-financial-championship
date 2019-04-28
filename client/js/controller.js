95340c2a0cf9e7ffb5e2fe40669c6e4f7b458e31
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-04-27 22:37:51 -0700

    End the fight

--- client/js/controller.js
+++ client/js/controller.js
@@ -5,15 +5,32 @@ export default class Controller {
 
 	constructor() {
 		this.world = new World(this);
-		this.fight = new Fight(this, this.world.player, this.world.others[0]);
+		this.fight = null;
+
+		this.startFight();
 	}
 
 	update(dt) {
 		this.world.update(dt);
-		this.fight.update(dt);
+		if (this.fight) {
+			this.fight.update(dt);
+			if (this.fight.done) {
+				this.fight = null;
+				document.querySelector('.fight').classList.remove('fight-shown');
+			}
+		}
+	}
+
+	startFight(player1, player2) {
+		this.fight = new Fight(this, player1, player2);
+		document.querySelector('.fight').classList.add('fight-shown');
 	}
 
 	onMouseClick(clickPos) {
+		if (this.fight) {
+			// Don't pass on input if we're in a fight baby
+			return;
+		}
 		this.world.onMouseClick(clickPos);
 	}
 
@@ -25,13 +42,18 @@ export default class Controller {
 		this.world.canvasManager.clear();
 		this.world.render();
 
-		this.fight.canvasManager.clear();
-		this.fight.render();
+		if (this.fight) {
+			this.fight.canvasManager.clear();
+			this.fight.render();
+		}
 	}
 
 	resize() {
 		this.world.canvasManager.resize();
-		this.fight.canvasManager.resize();
+
+		if (this.fight) {
+			this.fight.canvasManager.resize();
+		}
 	}
 
 }
