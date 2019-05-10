38003a990adafd6ec0548a957299c7943ac1ca8b
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2019-05-09 18:35:08 -0700

    Fix up the zip script

--- client/script/to-zip.sh
+++ client/script/to-zip.sh
@@ -1,14 +1,20 @@
 npm run webpack
 
 rm -rf /tmp/ufc
+rm -rf /tmp/ufc.zip
+rm -rf build/ufc.zip
 
 mkdir /tmp/ufc
 
 cp *.html /tmp/ufc
+mkdir /tmp/ufc/build
 cp build/main.bundle.js /tmp/ufc/build/main.bundle.js
 cp -r css /tmp/ufc/css
 cp -r font /tmp/ufc/font
 cp -r sfx /tmp/ufc/sfx
 cp -r assets /tmp/ufc/assets/
 
-zip -r build/ufc.zip /tmp/ufc
+cd /tmp
+zip -r ufc.zip ufc
+cd -
+cp /tmp/ufc.zip build/ufc.zip
